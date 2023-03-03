b0e9254d7830e0f59ba752c8e9ed6f418da49048
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-03-03 09:57:03 -0500

    Update and rename aosp_j8y18lte.mk to aicp_j8y18lte.mk

--- aicp_j8y18lte.mk
+++ aicp_j8y18lte.mk
@@ -17,13 +17,12 @@ LOCAL_PATH := device/samsung/j8y18lte
 # Inherit from those products, Most specific first.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit common Lineage phone.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := aosp_j8y18lte
+PRODUCT_NAME := aicp_j8y18lte
 PRODUCT_DEVICE := j8y18lte
 PRODUCT_MODEL := SM-J810G
 PRODUCT_BRAND := samsung
